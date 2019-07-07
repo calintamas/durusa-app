bb55107f74be428ac96424032743307cbf7e0767
Calin Tamas <calinntamas@gmail.com>
2019-07-07 10:40:48 +0300

    update initial setup action

--- src/modules/initial-setup/index.js
+++ src/modules/initial-setup/index.js
@@ -2,13 +2,11 @@ import React, { useEffect } from 'react'
 import { View } from 'react-native'
 
 import LoadingIndicator from '../../components/loading-indicator'
-import Navigation from '../../services/navigation'
 import styles from './styles'
 
 const InitialSetup = (props) => {
   useEffect(() => {
-    props.initialSetup();
-    Navigation.setRef(props.navigation);
+    props.initialSetup(props.navigation);
   }, []);
 
   return (
--- src/redux/app/actions.js
+++ src/redux/app/actions.js
@@ -1,14 +1,13 @@
 import appAC from './action-creators'
 import Navigation from '../../services/navigation'
 
-const initialSetup = () => (dispatch) => {
+const initialSetup = (navigationRef) => (dispatch) => {
   try {
     dispatch(appAC.initialSetup.pending());
 
-    setTimeout(() => {
-      dispatch(appAC.initialSetup.success());
-      Navigation.getRef().navigate('Default');
-    }, 2000)
+    Navigation.setRef(navigationRef);
+    Navigation.getRef().navigate('Default');
+    dispatch(appAC.initialSetup.success());
   } catch (err) {
     dispatch(appAC.initialSetup.error(err));
   }
